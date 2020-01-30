73ce4b1e514ba1b2e6d19fb113229ec966618865
Jacob VanWormer <sharpiejacob@gmail.com>
2020-01-30 11:39:46 -0800

    added components

--- user-dashboard/src/App.js
+++ user-dashboard/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './compone/Navbar';
+import ProductList from './compone/ProductList';
+import Details from './compone/Details';
+import Cart from './compone/Cart';
+import Default from './compone/Default';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar></Navbar>
+      <ProductList></ProductList>
+      <Details></Details>
+      <Cart></Cart>
+      <Default></Default>
+    </React.Fragment>
   );
 }
 
