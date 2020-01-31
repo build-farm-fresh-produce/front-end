2174e7502026bd2623b6f0745971644e574a46aa
Jane Suh <sourcherryyogurt@gmail.com>
2020-01-30 20:20:23 -0500

    Renamed cartActions.js

--- farm-fresh/src/reducers/cartReducer.js
+++ farm-fresh/src/reducers/cartReducer.js
@@ -1,4 +1,4 @@
-import { ADD_PRODUCT, UPDATE_CART, DELETE_FROM_CART } from '../actions';
+import { ADD_PRODUCT, UPDATE_CART, DELETE_FROM_CART } from '../actions/cartActions';
 
 const initialState = {
     cart: []
