38f23927ca1d1cd4f3beb638a29cb8c437aee44f
adella <scratchglory@gmail.com>
2020-02-03 21:42:05 -0500

    added a farmer dashboard and user edit form

--- farm-fresh/src/App.js
+++ farm-fresh/src/App.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Login';
-import Registration from './components/Registration';
-import Home from './components/Home';
-import Products from './components/Products';
-import Cart from './components/Cart';
-import  { Route, Link, Redirect } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-
-
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+import FarmerDasboard from "./components/FarmerDashboard";
+import EditForm from "./components/EditForm";
+import { Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-
   const PrivateRoute = ({ component: Products, ...rest }) => (
-    <Route {...rest} render={(props) => 
-        localStorage.getItem('token') ? (
-            <Products {...props} />
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem("token") ? (
+          <Products {...props} />
         ) : (
-            <Redirect to='/login-user' />
+          <Redirect to="/login-user" />
         )
-    } />
-)
+      }
+    />
+  );
 
   return (
     <Router>
       <div className="App">
-
-      <Route exact  path='/' component={Home} />
-      <Route  path='/login-user' component={Login} />
-      <Route  path='/register-user' component={Registration} />
-      <PrivateRoute  path='/products' component={Products} />
-      <PrivateRoute path='/cart' component={Cart} />
-    
+        <Route exact path="/" component={Home} />
+        <Route path="/login-user" component={Login} />
+        <Route path="/register-user" component={Registration} />
+        <Route path="/farmer-dashboard" component={FarmerDasboard} />
+        <Route path="/edit-info" component={EditForm} />
+        <PrivateRoute path="/products" component={Products} />
+        <PrivateRoute path="/cart" component={Cart} />
       </div>
     </Router>
-    
-    
   );
 }
 
---
+++ farm-fresh/src/components/FarmerDashboard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Faker from "faker";
+import { Link, Route } from "react-router-dom";
+// import EditForm from "./EditForm";
+
+export default function Farmer() {
+  return (
+    <div>
+      <h1>Farmer's Dashboard</h1>
+      <div className="info">
+        <p>Name: {Faker.company.companyName()}</p>
+        <p>Phone Number: {Faker.phone.phoneNumber()} </p>
+        <p>Email: {Faker.internet.email()}</p>
+        <p>Address: {Faker.address.streetAddress()}</p>
+        <Link to="/edit-info">Edit Info</Link>
+      </div>
+    </div>
+  );
+}
