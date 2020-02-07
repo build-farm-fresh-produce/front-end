467763c290b2a037c71d133b171024fed098ee06
adella <scratchglory@gmail.com>
2020-02-06 22:15:33 -0500

    partially finished editform

--- farm-fresh/src/components/EditForm.js
+++ farm-fresh/src/components/EditForm.js
@@ -6,14 +6,24 @@ import { axiosWithAuth } from "../tools/axiosAuth";
 
 export default function EditForm(props) {
   const farmerInfo = props.location.state.farmer;
-  console.log("about", props.location);
-  console.log("about", farmerInfo.farm_name);
+  const [updateInfo, setUpdateInfo] = useState({ ...farmerInfo });
+
+  const formHandler = event => {
+    event.preventDefault();
+  };
+
+  const handleNameChange = event => {
+    // debugger;
+    setUpdateInfo({ ...updateInfo, [event.target.name]: event.target.value });
+    console.dir(event.target);
+  };
+
   return (
     <div className="edit-form">
       <NavigationFarmer />
       <h1>Farmer's Dashboard</h1>
       <div className="info-edit">
-        <p>Farm Name: {farmerInfo.farm_name}</p>
+        <p>Farm Name: {updateInfo.farm_name}</p>
         <p>Phone Number: {farmerInfo.phone_number} </p>
         <p>Email: {farmerInfo.email}</p>
         <p>Address: {farmerInfo.address}</p>
@@ -21,6 +31,34 @@ export default function EditForm(props) {
         <p>State: {farmerInfo.state}</p>
         <p>Zip Code: {farmerInfo.zipcode}</p>
       </div>
+
+      <form onSubmit={formHandler}>
+        <div>
+          <label>
+            Name
+            <input
+              type="text"
+              name="farm_name"
+              value={updateInfo.farm_name}
+              onChange={handleNameChange}
+              placeholder={updateInfo.farm_name}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Email:
+            <input
+              type="text"
+              name="email"
+              value={updateInfo.email}
+              onChange={handleNameChange}
+              placeholder={updateInfo.email}
+            />
+          </label>
+        </div>
+        <button>Update your !@$#%@</button>
+      </form>
     </div>
   );
 }
